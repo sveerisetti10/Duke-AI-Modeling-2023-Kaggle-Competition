55ad53a7ebfc5b5e8ec19c5b677300c06cf8cb73
Sri Veerisetti <98369110+sveerisetti10@users.noreply.github.com>
2023-11-01 11:39:59 -0500

    Update Modeling_update.py

--- Final_Submission_Kaggle/Modeling_update.py
+++ Final_Submission_Kaggle/Modeling_update.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split,cross_val_score,KFold,GridSearchCV
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, LogisticRegression, LogisticRegressionCV
 from sklearn.neighbors import KNeighborsClassifier
